76497c53c3842192ec6eb7e6f5c1638d9c78727f
offlineprogrammer <mmalaka@gmail.com>
2024-05-20 20:11:38 -0700

    FrontEnd changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,154 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  Authenticator,
+  Button,
+  Text,
+  TextField,
+  Heading,
+  Flex,
+  View,
+  Image,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "../amplify_outputs.json";
+import { generateClient } from "aws-amplify/data";
+import { getUrl } from "aws-amplify/storage";
+import { uploadData } from "aws-amplify/storage";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
 
-function App() {
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await client.models.Note.list();
+    console.log(apiData);
+    const { data: notes } = await client.models.Note.list();
+    await Promise.all(
+      notes.map(async (note) => {
+        if (note.image) {
+          const linkToStorageFile = await getUrl({
+            path: `media/${note.image}`,
+          });
+          console.log(linkToStorageFile.url);
+          note.image = linkToStorageFile.url;
+        }
+        return note;
+      })
+    );
+    console.log(notes);
+    setNotes(notes);
+  }
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    console.log(form.get("image").name);
+
+    const { data: newNote } = await client.models.Note.create({
+      name: form.get("name"),
+      description: form.get("description"),
+      image: form.get("image").name,
+    });
+    console.log(newNote);
+    if (newNote.image)
+      await uploadData({
+        path: `media/${newNote.image}`,
+        data: form.get("image"),
+      });
+
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id }) {
+    const toBeDeletedNote = {
+      id: id,
+    };
+
+    const { data: deletedNote } = await client.models.Note.delete(
+      toBeDeletedNote
+    );
+    console.log(deletedNote);
+
+    fetchNotes();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+    <Authenticator>
+      {({ signOut }) => (
+        <div className="App">
+          <Heading level={1}>My Notes App</Heading>
+          <View as="form" margin="3rem 0" onSubmit={createNote}>
+            <Flex direction="row" justifyContent="center">
+              <TextField
+                name="name"
+                placeholder="Note Name"
+                label="Note Name"
+                labelHidden
+                variation="quiet"
+                required
+              />
+              <TextField
+                name="description"
+                placeholder="Note Description"
+                label="Note Description"
+                labelHidden
+                variation="quiet"
+                required
+              />
+              <View
+                name="image"
+                as="input"
+                type="file"
+                style={{ alignSelf: "end" }}
+              />
 
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
+              <Button type="submit" variation="primary">
+                Create Note
+              </Button>
+            </Flex>
+          </View>
+          <Heading level={2}>Current Notes</Heading>
+          <View margin="3rem 0">
+            {notes.map((note) => (
+              <Flex
+                key={note.id || note.name}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Text as="strong" fontWeight={700}>
+                  {note.name}
+                </Text>
+                <Text as="span">{note.description}</Text>
+                {note.image && (
+                  <Image
+                    src={note.image}
+                    alt={`visual aid for ${notes.name}`}
+                    style={{ width: 400 }}
+                  />
+                )}
+                <Button variation="link" onClick={() => deleteNote(note)}>
+                  Delete note
+                </Button>
+              </Flex>
+            ))}
+          </View>
+          <Button onClick={signOut}>Sign Out</Button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
